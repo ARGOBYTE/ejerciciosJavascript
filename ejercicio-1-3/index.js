9a0abb4c3c894d540d40f47e6a6489e4a952e3f5
= <=>
2022-12-11 12:09:43 -0500

    Se añade carpeta ejercicio-1-3

---
+++ ejercicio-1-3/index.js
@@ -0,0 +1,16 @@
+/* 
+EJERCICIO 1-3:
+    * Crea una variable llamada "numero1" y otra llamada "numero2".
+    * Suma las dos variables y muestra el resultado en consola.
+    * Crea otras variables de tipo number y realiza operaciones de resta, multiplicación y división. Mostrando el resultado por consola.
+*/
+
+const numero1 = 23;
+const numero2 = 12;
+const numero3 = 2;
+const numero4 = 3;
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero3);
+console.log(numero3 * numero2);
+console.log(numero1 / numero4);
