271f11e3f1ec1fe015b6e21f65162936a3161315
= <=>
2022-12-13 18:45:24 -0500

    Se añade carpeta de ejercicio-1-4

---
+++ ejercicio-1-4/index.js
@@ -0,0 +1,11 @@
+/* 
+EJERCICIO 1-4:
+    * Crea una variable llamada "saludo" y asígnale un string que contenga hola.
+    * Comprueba con un if si el string "saludo" es igual a hola. Y saca por consola un mensaje que diga "Ha saludado".
+*/
+
+const saludo = 'hola';
+
+if(saludo == 'hola'){
+    console.log('Ha saludado')
+}
