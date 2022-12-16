6613d7bac59c1066c10c4276eb45dd8bfff45717
= <=>
2022-12-15 22:50:54 -0500

    Se adiciona la carpeta del ejercicio 2-3

---
+++ ejercicio-2-3/index.js
@@ -0,0 +1,11 @@
+/* 
+EJERCICIO 2-3:
+    * Crea una función que calcule el área de un rectángulo y la muestre por consola.
+*/
+
+function rectangulo(largo, ancho) {
+    return largo * ancho
+}
+
+const area = rectangulo(4, 10)
+console.log('El área del rectángulo es: ' + area + ' mts2')
