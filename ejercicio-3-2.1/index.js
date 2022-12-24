f25504852d7f39875ca7f60b67003b1d6ec578b1
= <=>
2022-12-24 11:37:39 -0500

    Add ejercicio 3-2.1 modified

--- ejercicio-3-2.1/index.js
+++ ejercicio-3-2.1/index.js
@@ -1,9 +1,9 @@
 /*
     EJERCICIO 3-2:
     * Escribe un programa que permita a alguien adivinar un PIN de 4 dígitos
-      exactamente 3 veces. Si el usuario adivina el número correctamente, imprime
+      las veces que desee. Si el usuario adivina el número correctamente, imprime
       "¡Eso fue correcto!" de lo contrario, imprimirá "Lo siento, eso estaba mal".
-      El programa se detiene despues del 4° intento o si lo adivinaron correctamente.
+      El programa se detiene despues de escribir "salir" o si lo adivinaron correctamente.
 
       Puedes usar este código para obtener el PIN de entrada del usuario:
 
@@ -22,6 +22,9 @@ let aviso = true;
 while (aviso == true) {
     if (adivina === pin) {
         console.log("¡Eso fue correcto!");
+        aviso = false;
+    } else if (adivina === 'salir') {
+        console.log("Adios!!");
         aviso = false;
     } else {
         console.log("Lo siento, eso estaba mal");
